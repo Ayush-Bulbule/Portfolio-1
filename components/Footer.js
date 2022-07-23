7e47211a68951d8274a9c7513c1219061de14273
Ayush Bulbule <ayushbulbule24@gmail.com>
2022-07-24 02:00:55 +0530

    Footer Added

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="flex items-center flex-col text-center md:px-40 px-8 py-8 bg-black text-white">
+            <p>&copy;AyushBulbule 2022</p>
+            <p>Made with ❤️</p>
+            <p>For More visit: Ayush-Bulbule@Github</p>
+        </div>
     )
 }
 
