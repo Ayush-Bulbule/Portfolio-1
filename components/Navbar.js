ce4dd5d6449f05104c86f9d35cae9d2e488aed46
Ayush Bulbule <ayushbulbule24@gmail.com>
2022-07-23 15:56:09 +0530

    Hero Section Done!

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,182 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Transition, transition } from '@headlessui/react'
+import { Link } from 'react-scroll'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { ImSun } from 'react-icons/im'
+import { FaRegMoon } from 'react-icons/fa'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [isDark, setIsDark] = useState(false)
     return (
-        <div>Navbar</div>
+        <div>
+            <nav className='fixed z-20 bg-white w-full border border-b-gray-300'>
+                <div className="w-full">
+                    <div className="flex items-center h-20 w-full">
+                        <div className="flex items-center justify-between w-full sm:mx-10 md:mx20">
+                            {/* Brand Logo Section */}
+                            <div className="flex items-center justify-center flex-shrink-0 sm:ml-4">
+                                <h1 className='text-xl font-bold ml-8'>Ayush <span className="text-blue-600">Bulbule</span> </h1>
+                            </div>
+
+                            {/* Links Section */}
+                            <div className="hidden md:block">
+                                {/* style={{ fontFamily: "'Maven Pro', sans-serif" }} */}
+                                <div className="ml-10 space-x-4 flex items-baseline font-semibold" >
+                                    <Link
+                                        href='/home'
+                                        action='home'
+                                        to='home' smooth={true}
+                                        offset={50} duration={500}
+                                        className='text-md text-gray-500 hover:text-blue-500 px-3 py-2 cursor-pointer'>Home</Link>
+                                    <Link
+                                        href="/services"
+                                        activeClass="services"
+                                        to="services"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className='text-md text-gray-500 hover:text-blue-500 px-3 py-2 cursor-pointer'>Services</Link>
+                                    <Link
+
+                                        action='Projects'
+                                        to='projects'
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500} className='text-md text-gray-500 hover:text-blue-500 px-3 py-2 cursor-pointer'>Projects</Link>
+                                    <Link
+                                        href="/contact"
+                                        activeClass="contact"
+                                        to="contact"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500} className='text-md text-gray-500 hover:text-blue-500 px-3 py-2 cursor-pointer'>Contact</Link>
+                                </div>
+
+                            </div>
+                            {/* Section for direct contact via email */}
+                            <div className="flex justify-center items-center pr-8 flex-shrink-0 invisible md:visible cursor-pointer">
+                                {isDark ? <ImSun className='font-semibold text-2xl mr-1 text-white' /> : <FaRegMoon className='font-semibold text-2xl mr-1' />}
+                                <h3 className="text-lg font-semibold">
+                                    Colormode
+                                </h3>
+                                {/* <a href='https://wa.me/918459320663?text=hi' className="text-xl font-bold cursor-pointer ">Say <span className='text-blue-600'>hi</span></a> */}
+                            </div>
+                        </div>
+
+                        {/* Desktop Part Completed */}
+                        {/* Mobile part  */}
+
+                        <div className="w-1/5  flex md:hidden">
+                            <button
+                                onClick={() => setIsOpen(!isOpen)}
+                                type="button"
+
+                                className="bg-blue-600 inline-flex items-center justify-center p-2 rounded-md text-white  hover:bg-black focus:outline-none focus:ring-white"
+                                aria-controls="mobile-menu"
+                                aria-expanded="false"
+                            >
+                                <div className="sr-only">Oprn Menu</div>
+                                {isOpen ? <FaTimes className='text-white' /> : <FaBars className='text-white' />}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Menu */}
+                <Transition show={isOpen}
+                    enter="transition ease-out duration-100 transform"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="transition ease-in duration-75 transform"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                >
+                    {
+                        (ref) => (
+                            <div className="md:hidden" id="mobile-menu">
+                                <div
+                                    ref={ref}
+                                    className="bg-white mx-4 pt-4 pb-4 space-y-1"
+                                >
+
+                                    <Link
+                                        href="/home"
+                                        activeClass="home"
+                                        to="home"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                    >
+                                        Home
+                                    </Link>
+                                    <Link
+                                        href="/services"
+                                        activeClass="services"
+                                        to="services"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                    >
+                                        Services
+                                    </Link>
+
+                                    <Link
+                                        href="/work"
+                                        activeClass="projects"
+                                        to="projects"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                    >
+                                        Projects
+                                    </Link>
+                                    <Link
+                                        href="/techs"
+                                        activeClass="techs"
+                                        to="techs"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                    >
+                                        Techcs
+                                    </Link>
+
+                                    <Link
+                                        href="/contact"
+                                        activeClass="contact"
+                                        to="contact"
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                    >
+                                        Contact
+                                    </Link>
+
+                                    <a
+                                        href='https://wa.me/918459320663?text=hi'
+                                        smooth={true}
+                                        offset={50}
+                                        duration={500}
+                                        className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                    >
+                                        Say hi👋
+                                    </a>
+                                </div>
+                            </div>
+                        )
+                    }
+
+                </Transition>
+
+
+            </nav >
+        </div >
     )
 }
 
--- pages/Hero.js
+++ pages/Hero.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
+
+import profile from '../public/images/profile.png'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className="flex mx-12  justify-start items-center md:pt-28 pt-28 md:mx-40">
+            {/* Block 1: Text */}
+            <div className="w-flex flex-col items-center justify-start h-90">
+                <h1 className="font-bold text-6xl text-left">Hello, I am <span className="text-sky-600">Ayush</span></h1>
+                <p className='text-md mt-8 text-gray-800 text-start '>Building a successful product is a challenge. I am highly energetic in user experience design, interfaces and web development.</p>
+                <p className='my-5 font-semibold text-gray-600'>📍Akola, Maharashtra</p>
+                <button className="h-12 flex items-center px-8 mt-7 bg-blue-700 rounded-full text-white font-semibold">See my portfolio!</button>
+            </div>
+            {/* Block 2: Image */}
+            <div className=" items-center hidden md:flex justify-end md:py-16 md:w-3/4">
+                <div className="relative rounded-full w-64 h-64 shadow-md shadow-sky-200">
+                    <Image src={profile} />
+                </div>
+            </div>
+        </div>
     )
 }
 
